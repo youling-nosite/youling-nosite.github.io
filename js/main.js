f6256c5d643726326302758aae3a66c8d702132e
youling-nosite <yoiyoi0905@gmail.com>
2026-04-19 05:49:32 +0800

    26.0419.20

--- js/main.js
+++ js/main.js
@@ -3,19 +3,23 @@ const toast = document.getElementById("toast");
 
 const email = "yoiyoi0905@email.com";
 
-emailBtn.addEventListener("click", (e) => {
+emailBtn.addEventListener("click", async (e) => {
   e.preventDefault();
 
-  navigator.clipboard.writeText(email);
+  try {
+    await navigator.clipboard.writeText(email);
+
+    showToast("已成功複製到剪貼簿");
+  } catch (err) {
+    showToast("複製失敗 😢");
+  }
+});
+
+function showToast(message) {
+  toast.textContent = message;
   toast.classList.add("show");
 
   setTimeout(() => {
     toast.classList.remove("show");
   }, 2000);
-});
-
-fetch("\\components\\navbar.html")
-    .then(res => res.text())
-    .then(data => {
-        document.getElementById("navbar").innerHTML = data;
-    }); 
+}

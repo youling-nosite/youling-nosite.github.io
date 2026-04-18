a00a00b666b09a7915e3a0db85ca394129469b5c
youling-nosite <yoiyoi0905@gmail.com>
2026-04-19 05:46:34 +0800

    26.0419.19

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+const emailBtn = document.getElementById("copy-email");
+const toast = document.getElementById("toast");
+
+const email = "yoiyoi0905@email.com";
+
+emailBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  navigator.clipboard.writeText(email);
+  toast.classList.add("show");
+
+  setTimeout(() => {
+    toast.classList.remove("show");
+  }, 2000);
+});
+
 fetch("\\components\\navbar.html")
     .then(res => res.text())
     .then(data => {
